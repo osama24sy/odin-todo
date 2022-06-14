f33ed6c3bb05762e2e8de32580b39ba4f969633f
Osama Shbib <osamashbib28@gmail.com>
2022-06-14 11:13:04 +0300

    minor changes

---
+++ src/assets/pages/header.js
@@ -0,0 +1,8 @@
+const createHeader = () => {
+    const body = document.getElementsByTagName("body");
+
+    const header = document.createElement("div");
+    header.classList.add("header");
+
+    
+};
