bbd71a45202bfcedca06303204aa60c004cc45ae
Osama Shbib <osamashbib28@gmail.com>
2022-06-21 16:24:22 +0300

    Add Project Renderer

--- src/assets/modules/projects.js
+++ src/assets/modules/projects.js
@@ -4,19 +4,84 @@ const createProjectSecion = () => {
     const cont = document.createElement('div');
     cont.id = 'project-section';
 
+    const innerCont = document.createElement('div');
+    innerCont.id = 'inner-project';
+
+    
+    cont.appendChild(innerCont);
+
+    content.appendChild(cont);
+
+    renderProjects();
+};
+
+const renderProjects = () => {
+    const innerCont = document.getElementById('inner-project');
+
+    innerCont.innerHTML = '';
+
     for (let i = 0; i < localStorage.length; i++) {
         const projectName = localStorage.key(i);
-        
         const projectContainer = document.createElement('div');
         const projectTitle = document.createElement('p');
         projectTitle.innerText = `${projectName}`;
 
         projectContainer.appendChild(projectTitle);
 
-        cont.appendChild(projectContainer);
+        innerCont.appendChild(projectContainer);
     }
 
-    content.appendChild(cont);
 };
 
-export default createProjectSecion;
+const addProjectForm = () => {
+    const projectSection = document.getElementById('project-section');
+
+    const addBtn = document.createElement('button');
+    addBtn.type = 'button';
+    addBtn.innerText = 'Add a Project';
+
+    addBtn.addEventListener('click', () => {
+        addBtn.style.display = 'none';
+        form.style.display = 'initial';
+    });
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Project Name';
+    input.id = 'project-input';
+
+    const confirmBtn = document.createElement('button');
+    confirmBtn.type = 'button';
+    confirmBtn.innerText = 'Confirm';
+    
+    const cancelBtn = document.createElement('button');
+    cancelBtn.type = 'button';
+    cancelBtn.innerText = 'Cancel';
+    
+    cancelBtn.addEventListener('click', () => {
+        addBtn.style.display = 'initial';
+        form.style.display = 'none';
+    });
+    
+    const form = document.createElement('div');
+    form.id = 'add-project-form';
+    
+    form.style.display = 'none';
+
+    confirmBtn.addEventListener('click', addProjectToStorage);
+    
+    form.appendChild(input);
+    form.appendChild(confirmBtn);
+    form.appendChild(cancelBtn);
+
+    projectSection.appendChild(addBtn);
+    projectSection.appendChild(form);
+};
+
+const addProjectToStorage = () => {
+    const input = document.getElementById('project-input');
+    localStorage.setItem(input.value, JSON.stringify([]));
+    renderProjects();
+};
+
+export {createProjectSecion, addProjectForm};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import '../src/assets/style/style.css';
 import createHeader from "./assets/modules/header";
 import createTodoSection from './assets/modules/todo-section/todo-section';
 import renderTodos from './assets/modules/todo-renderer';
-import createProjectSecion from './assets/modules/projects';
+import {createProjectSecion, addProjectForm} from './assets/modules/projects';
 
 const body = document.getElementsByTagName("body")[0];
 const content = document.createElement("div");
@@ -14,3 +14,4 @@ createHeader();
 createTodoSection();
 renderTodos();
 createProjectSecion();
+addProjectForm();
